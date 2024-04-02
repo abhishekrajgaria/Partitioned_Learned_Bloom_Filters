377c6c6df6c6aa098ea13631b52eb9fa72590985
abhishekrajgaria <abhishek17276@iiitd.ac.in>
2024-04-02 00:04:56 -0600

    added code

---
+++ model.py
@@ -0,0 +1,17 @@
+import torch.nn as nn
+
+class CharacterLevelGRU(nn.Module):
+    def __init__(self, vocab_size, hidden_size, embedding_dim):
+        super(CharacterLevelGRU, self).__init__()
+        self.embedding = nn.Embedding(vocab_size, embedding_dim)
+        self.gru = nn.GRU(embedding_dim, hidden_size)
+        self.fc = nn.Linear(hidden_size, 1)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        embedded = self.embedding(x)
+        print(embedded.shape())
+        output, _ = self.gru(embedded.view(len(x), 1, -1))
+        output = self.fc(output.view(len(x), -1))
+        output = self.sigmoid(output)  # Apply sigmoid activation function
+        return output
